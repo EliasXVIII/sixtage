f0a0e8f1ac392fef32690cf505d9d955deb320fd
JesusDavid16 <jesusdavidr251@gmail.com>
2024-02-15 13:21:18 +0100

    añadi una nueva carpeta js y modifique el css y tambien hice codigos de javascript

--- jesus.js
+++ jesus.js
@@ -1,10 +1,25 @@
-const loginPage = document.querySelector('#logingJ')
-
-loginPage.addEventListener('submit',(e)=>{
+const registroFormJ = document.querySelector('#registroFormJ')
+registroFormJ.addEventListener('submit',(e)=>{
     e.preventDefault()
 
-const usuario = document.querySelector('#usuario')
-const contraseña = document.querySelector('#contraseña')
+const mailJ = document.querySelector('#mailJ').value
+const nameJ = document.querySelector('#nameJ').value
+const lastnameJ = document.querySelector('#lastnameJ').value
+const telefonoJ = document.querySelector('#telefonoJ').value
+const passwordJ = document.querySelector('#passwordJ').value
 
 const users = JSON.parse(localStorage.getItem('users')) || []
+const usersRegister = users.find(user => user.email === mailJ)
+
+if(usersRegister){
+    return alert('El usuario ya esta registrado!')
+}
+
+users.push({email: mailJ, name: nameJ, lastname: lastnameJ, telefono: telefonoJ, password: passwordJ})
+localStorage.setItem('users',JSON.stringify(users))
+alert('Registro exitoso!')
+window.location.href = 'acceso.html'
+
+//////RE DERECCION A LOGING/////
+
 })
