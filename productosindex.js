88f8e2d271c432ae43e73d6e6b23da90521b5546
l6898 <espinolalourdes83@gmail.com>
2024-02-19 11:30:14 +0100

    pagina productos; style desde la 1239 y productosindex.js

---
+++ productosindex.js
@@ -0,0 +1,6 @@
+const btnCart= document.querySelector('.contenedorcarroicono')
+const contenedorCarroProducto = document.querySelector('.contenedorcarroproducto')
+
+btnCart.addEventListener('click', ()=>{
+    contenedorCarroProducto.classList.toggle ('hiddencarro')
+})
