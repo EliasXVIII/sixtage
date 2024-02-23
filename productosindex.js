0cf9756512618dfeb2ccb9c60f3394976bb6ad3b
l6898 <espinolalourdes83@gmail.com>
2024-02-23 14:38:57 +0100

    cambio para arreglar

--- productosindex.js
+++ productosindex.js
@@ -9,23 +9,23 @@ btnCart.addEventListener('click', ()=>{
 
 
 
-const carroInfo = document.querySelector ('.carroproducto');
-const columnaCarro = document.querySelector ('.columnacarro');
+const carroInfo = document.querySelector('.carroproducto');
+const columnaCarro = document.querySelector('.columnacarro');
 
 /* lista de contenedores de productos */
-const listaDeProductos = document.querySelector ('.contenedor-items')
+const listaDeProductos = document.querySelector('.contenedor-items')
 
 /* variable de arreglo de productos */
 let productosTodos = [];
 
-const valorTotal = document.querySelector ('.total-pagar');
+const valorTotal = document.querySelector('.total-pagar');
 
 const contadorProductos = document.getElementById('#contadorproductos');
 
-const carroVacio = document.querySelector ('.carrovacio');
-const carroTotal = document.querySelector ('.total-carrito');
+const carroVacio = document.querySelector('.carrovacio');
+const carroTotal = document.querySelector('.total-carrito');
  
-listaDeProductos.addEventListener ('click', e => {
+listaDeProductos.addEventListener('click', e =>{
     if(e.target.classList.contains('btnadd')){
     const producto = e.target.parentElement;
     
@@ -36,15 +36,15 @@ listaDeProductos.addEventListener ('click', e => {
         price: producto.querySelector('p').textContent,
     };
 
-    console.log(infoProducto)
+   /*  console.log(infoProducto) */
     const exits = productosTodos.some(
         producto => producto.title === infoProducto.title
     );
-    console.log(exits)
+   /*  console.log(exits) */
     
     
     if (exits){
-        const productos = productosTodos.map(producto => {
+        const productos = productosTodos.map(producto =>{
             if (producto.title === infoProducto.title){
                 producto.quantity++;
                 return producto;
@@ -60,15 +60,16 @@ listaDeProductos.addEventListener ('click', e => {
     }
 });   
 
-columnaCarro.addEventListener ('click', (e) => {
-    if(e.target.classList.contains ('price')){
+columnaCarro.addEventListener('click', e => {
+    if(e.target.classList.contains('iconocruz')) {
         const producto = e.target.parentElement;
-        const title = producto.querySelector('p').texContent;
+        const title = producto.querySelector('p').textContent;
 
-        productosTodos = productosTodos.filter (
-            product => product.title !==title
+        productosTodos = productosTodos.filter(
+            producto => producto.title !== title
         );
         console.log(productosTodos);
+
         showHTML();
     }
 });
@@ -76,16 +77,14 @@ columnaCarro.addEventListener ('click', (e) => {
 
  const showHTML = () => {
 
-   
-    
-    if(!productosTodos.length){
+    if(!productosTodos.length) {
         carroVacio.classList.remove('hidden');
-        columnaCarro.classList.add ('hidden');
-        carroTotal.classList.add ('hidden');
+        columnaCarro.classList.add('hidden');
+        carroTotal.classList.add('hidden');
     }else {
-        carroVacio.classList.add ('hidden');
+        carroVacio.classList.add('hidden');
         columnaCarro.classList.remove('hidden');
-        carroTotal.classList.remove ('hidden');
+        carroTotal.classList.remove('hidden');
     } 
  
     /* limpieza html
@@ -96,10 +95,10 @@ columnaCarro.addEventListener ('click', (e) => {
     let totalDeProductos = 0;
 
      productosTodos.forEach(producto => {
-        const contenedorproducto = document.createElement('div');
-        contenedorproducto.classList.add('carroproducto');
+        const contenedorProducto = document.createElement('div');
+        contenedorProducto.classList.add('carroproducto');
 
-        contenedorproducto.innerHTML = `
+        contenedorProducto.innerHTML = `
         <div class="infocarroproducto">
                    <span class="cantidad-producto-carrito">${producto.quantity}</span>
                    <p class="titulo-info-carrito">${producto.title}</p>
@@ -108,13 +107,12 @@ columnaCarro.addEventListener ('click', (e) => {
         <img class="iconocruz" src="logo+iconos/cruz.svg" alt="">`;
 
 
-        columnaCarro.append(contenedorproducto);
+        columnaCarro.append(contenedorProducto);
         total = 
             total + parseInt(producto.quantity * producto.price.slice(1));
-            console.log(total)
-            totalDeProductos = totalDeProductos + producto.quantity;
+        totalDeProductos = totalDeProductos + producto.quantity;
     });
-         valorTotal.innerText =`$ ${total}`;
+         valorTotal.innerText = `$ ${total}`;
          contadorProductos.innerText = totalDeProductos;
 };
 
