5e58538f7f8d8624bc2b668d28012273178f4b9f
EliasXVIII <elias.renault4@gmail.com>
2024-02-26 13:18:54 +0100

    cambio de tienda, agregar productos. carpeta JS

---
+++ js/app.js
@@ -0,0 +1,69 @@
+const shopContent = document.getElementById("shopContent");
+const verCarrito = document.getElementById("verCarrito");
+const modalContainer = document.getElementById("modal-container");
+const cantidadCarrito = document.getElementById("cantidadCarrito");
+
+let carrito = JSON.parse(localStorage.getItem("carrito")) || []; //carrito vacio
+
+productos.forEach((product) => {                 
+    let content = document.createElement("div"); 
+    content.className= "card";
+    content.innerHTML = `
+        <img src="${product.img}">                
+        <h3>${product.nombre}</h3>
+        <p> ${product.precio} €</p>
+        `;
+
+    shopContent.append(content); 
+
+    let comprar = document.createElement("button")
+    comprar.className="comprar"
+    comprar.innerText = "Comprar";
+
+    content.append(comprar)
+
+    comprar.addEventListener("click", ()=>{
+        const repeat = carrito.some((repeatProduct) => repeatProduct.id === product.id);
+        
+        if(repeat){
+            carrito.map((prod)=>{
+                if(prod.id === product.id){
+                    prod.cantidad++;
+                }
+            });
+        
+
+        
+
+
+        }else{
+        carrito.push({
+            id: product.id,
+            img: product.img,
+            nombre: product.nombre,
+            precio: product.precio,
+            cantidad: product.cantidad,
+
+        });
+        }
+    carritoCounter();
+    saveLocal();
+    });
+
+
+});
+
+//utilizo .forEach para recorrer el array
+//la palabra "product" la declaro en el .forEach para luego llamar a los elementos
+//con append le digo que shopContent retenga todo lo que tiene content
+
+//set item para enviar info a Local Storage
+const saveLocal = () => {
+    localStorage.setItem("carrito", JSON.stringify(carrito));
+};
+
+
+//Get item para obtener esa info.
+
+JSON.parse(localStorage.getItem("carrito"))
+
---
+++ js/products.js
@@ -0,0 +1,32 @@
+const productos = [
+    {   id: 1,
+        nombre: "zapatos", 
+        precio: 100,
+        img: "/img/zapatos.jpg",
+        cantidad: 1,
+    },
+    {
+        id: 2,
+        nombre: "pantalon",
+        precio: 50,
+        img: "/img/pantalon.jpg",
+        cantidad: 1,
+    
+    },
+    {
+        id: 3,
+        nombre: "camiseta", 
+        precio: 30,
+        img: "/img/camiseta.jpg",
+        cantidad: 1,
+    
+    },
+    {
+        id: 4,
+        nombre: "jersey", 
+        precio: 40,
+        img: "/img/jersey.jpg",
+        cantidad: 1,
+    },
+   
+];
