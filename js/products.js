9a94ef7e0867ba59c828c3f388db5cb7dd352e1e
EliasXVIII <elias.renault4@gmail.com>
2024-02-26 14:24:30 +0100

    agregando carrito a todas las paginas

--- js/products.js
+++ js/products.js
@@ -1,13 +1,13 @@
 const productos = [
     {   id: 1,
-        nombre: "zapatos", 
+        nombre: "Zapatillas", 
         precio: 100,
         img: "/img/zapatos.jpg",
         cantidad: 1,
     },
     {
         id: 2,
-        nombre: "pantalon",
+        nombre: "Pantalon",
         precio: 50,
         img: "/img/pantalon.jpg",
         cantidad: 1,
@@ -15,7 +15,7 @@ const productos = [
     },
     {
         id: 3,
-        nombre: "camiseta", 
+        nombre: "Camiseta", 
         precio: 30,
         img: "/img/camiseta.jpg",
         cantidad: 1,
@@ -23,9 +23,37 @@ const productos = [
     },
     {
         id: 4,
-        nombre: "jersey", 
+        nombre: "Jersey", 
         precio: 40,
         img: "/img/jersey.jpg",
+        cantidad: 1,
+    },
+    {
+        id: 5,
+        nombre: "Blucher", 
+        precio: 40,
+        img: "/imgproductos/blucher.jpg",
+        cantidad: 1,
+    },
+    {
+        id: 6,
+        nombre: "Camiseta cuello azul", 
+        precio: 40,
+        img: "/imgproductos/camisetacuelloazul.jpg",
+        cantidad: 1,
+    },
+    {
+        id: 7,
+        nombre: "Lazo", 
+        precio: 40,
+        img: "/imgproductos/lazo.jpg",
+        cantidad: 1,
+    },
+    {
+        id: 8,
+        nombre: "Tacones", 
+        precio: 40,
+        img: "/imgproductos/tacones.jpg",
         cantidad: 1,
     },
    
